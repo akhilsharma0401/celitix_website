19977b615dd62a9132b0f8bd09ebd59a55535589
Arihant Jain <arihantj916@gmail.com>
2025-06-12 10:16:36 +0530

    feat: add sitemap script

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.celitix.com",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.9,
+  exclude: [],
+  alternateRefs: [],
+};
