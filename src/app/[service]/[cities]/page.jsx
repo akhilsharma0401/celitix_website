021950e3d560c63c303551ca4765b21932d8f1f0
Arihant Jain <arihantj916@gmail.com>
2025-06-26 14:56:01 +0530

    feat: add layout for cities Whatsapp

--- src/app/[service]/[cities]/page.jsx
+++ src/app/[service]/[cities]/page.jsx
@@ -1,5 +1,7 @@
 import { smskey, rcskey, whatsappkey } from "@/app/lib/cities";
+import Rcs from "@/app/rcs-business-messaging/Rcs";
 import Sms from "@/app/sms-marketing/sms";
+import WhatsApp from "@/app/whatsapp-business-platform/whatsapp";
 import { Seo } from "@/utils/seo";
 
 export default async function Cities({ params }) {
@@ -20,6 +22,17 @@ export default async function Cities({ params }) {
         : `localhost:3000/sms-marketing`,
     },
   };
+  const WhatsAppCitiesMeta = {
+    title: `WhatsApp Business API & Chatbots | Celitix ${formattedCity}`,
+    description: `Use WhatsApp to grow your ${formattedCity} business. Reach new heights with Celitix’s enterprise messaging solutions. Chatbots, Click to WhatsApp ads, CRM, & more.`,
+  };
+
+  // //
+  //   const RCSCitiesMeta = {
+  //     title: `RCS Business Messaging in ${formattedCity}`,
+  //     description:
+  //       "Break free from the limitations of SMS marketing. Engage users with buttons, media, and branded messages via Celitix’s RCS platform.",
+  //   };
 
   if (smskey?.includes(service)) {
     return (
@@ -35,20 +48,20 @@ export default async function Cities({ params }) {
   // if (rcskey?.includes(service)) {
   //   return (
   //     <>
-  //       {/* <Seo title={RCSMetaData.title} description={RCSMetaData.description} /> */}
+  //       <Seo title={RCSMetaData.title} description={RCSMetaData.description} />
   //       <Rcs />
   //     </>
   //   );
   // }
-  // if (whatsappkey?.includes(service)) {
-  //   return (
-  //     <>
-  //       {/* <Seo
-  //         title={WhatsAppMetaData.title}
-  //         description={WhatsAppMetaData.description}
-  //       /> */}
-  //       <WhatsApp />
-  //     </>
-  //   );
-  // }
+  if (whatsappkey?.includes(service)) {
+    return (
+      <>
+        <Seo
+          title={WhatsAppCitiesMeta.title}
+          description={WhatsAppCitiesMeta.description}
+        />
+        <WhatsApp />
+      </>
+    );
+  }
 }
