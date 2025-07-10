c29c047312b4dac3ff0fe6a466834af855a79134
akhilsharma0401 <akhilsharma0401@gmail.com>
2025-07-10 16:02:25 +0530

    10julynew

--- src/app/[service]/page.jsx
+++ src/app/[service]/page.jsx
@@ -5,28 +5,34 @@ import { smskey, rcskey, whatsappkey } from "../lib/cities";
 import ErrorPage from "../components/ErrorPage";
 import { Seo } from "@/utils/seo";
 
-const RCSMetaData = {
-  title: "RCS Business Messaging | Celitix",
-  description:
-    "Break free from the limitations of SMS marketing. Engage users with buttons, media, and branded messages via Celitix’s RCS platform.",
-};
-const WhatsAppMetaData = {
-  title: `WhatsApp Business API & Chatbots | Celitix`,
-  description: `Use WhatsApp to grow your business. Reach new heights with Celitix’s enterprise messaging solutions. Chatbots, Click to WhatsApp ads, CRM, & more.`,
-};
-const SMSMetaData = {
-  title: "SMS Marketing & OTP Platform | Celitix",
-  description:
-    "Reach anyone, anywhere, instantly. Use Celitix’s SMS platform to deliver OTPs, alerts, and promotional messages and drive results.",
-};
+
 
 export default async function SMS({ params }) {
   const { service } = await params;
 
+  const RCSMetaData = {
+    title: "RCS Business Messaging | Celitix",
+    description:
+      "Break free from the limitations of SMS marketing. Engage users with buttons, media, and branded messages via Celitix’s RCS platform.",
+    link: `https://www.celitix.com/${service}`,
+
+  };
+  const WhatsAppMetaData = {
+    title: `WhatsApp Business API & Chatbots | Celitix`,
+    description: `Use WhatsApp to grow your business. Reach new heights with Celitix’s enterprise messaging solutions. Chatbots, Click to WhatsApp ads, CRM, & more.`,
+    link: `https://www.celitix.com/${service}`,
+  };
+  const SMSMetaData = {
+    title: "SMS Marketing & OTP Platform | Celitix",
+    description:
+      "Reach anyone, anywhere, instantly. Use Celitix’s SMS platform to deliver OTPs, alerts, and promotional messages and drive results.",
+    link: `https://www.celitix.com/${service}`,
+  };
+
   if (smskey?.includes(service)) {
     return (
       <>
-        <Seo title={SMSMetaData.title} description={SMSMetaData.description} />
+        <Seo title={SMSMetaData.title} description={SMSMetaData.description} link={SMSMetaData.link} />
         <Sms />
       </>
     );
@@ -34,7 +40,7 @@ export default async function SMS({ params }) {
   if (rcskey?.includes(service)) {
     return (
       <>
-        <Seo title={RCSMetaData.title} description={RCSMetaData.description} />
+        <Seo title={RCSMetaData.title} description={RCSMetaData.description} link={RCSMetaData.link} />
         <Rcs />
       </>
     );
@@ -45,6 +51,7 @@ export default async function SMS({ params }) {
         <Seo
           title={WhatsAppMetaData.title}
           description={WhatsAppMetaData.description}
+          link={WhatsAppMetaData.link}
         />
         <WhatsApp />
       </>
