fb5829f5fe84ba56a3989060f79164e832856f4d
akhilsharma0401 <akhilsharma0401@gmail.com>
2025-07-18 11:48:36 +0530

    17julyupdate

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -139,7 +139,7 @@ export default function RootLayout({ children }) {
         <CookieConsent />
         <main>{children}</main>
         <Footer />
-        <ChatLauncher />
+        {/* <ChatLauncher /> */}
       </body>
     </html>
   );
