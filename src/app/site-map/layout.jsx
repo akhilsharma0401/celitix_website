d5f8a47137eabe86ff2f89ffc628f8b862e7a579
akhilsharma0401 <akhilsharma0401@gmail.com>
2025-12-16 10:43:30 +0530

    15dec

---
+++ src/app/site-map/layout.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  alternates: {
+    canonical: "https://www.celitix.com/site-map",
+  },
+};
+
+export default function Layout({ children }) {
+  return <>{children}</>;
+}
