8e09786e1b804b4308108c84f99b1b6fe68bed3f
akhilsharma0401 <akhilsharma0401@gmail.com>
2026-06-26 18:00:27 +0530

    7778

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -180,7 +180,7 @@ export default function RootLayout({ children }) {
           />
         </noscript>
 
-        <noscript> <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=9594316&fmt=gif" /> </noscript>
+        <noscript> <img height="1" width="1" alt="" src="https://px.ads.linkedin.com/collect/?pid=9594316&fmt=gif" /> </noscript>
         <script
           src="./widget.js"
           data-widget-id={`${process.env.NEXT_PUBLIC_WIDGET_ID}`}
